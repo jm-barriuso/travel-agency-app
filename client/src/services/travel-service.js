bdc6a60316c7d990749d7ff752d7ef7c7b00a680
José Miguel Barriuso <josemiguelbarriuso@Joses-MacBook-Pro.local>
2022-11-11 22:19:32 -0300

    cambio local host

--- client/src/services/travel-service.js
+++ client/src/services/travel-service.js
@@ -2,18 +2,18 @@
 import axios from 'axios';
 
 export const createTravel = (travel) => {
-    const newTravel = axios.post('http://localhost:8080/api/travel', travel);
+    const newTravel = axios.post('http://localhost:8000/api/travel', travel);
     return newTravel;
 }
 
-export const getAllTravels = () => axios.get('http://localhost:8080/api/travel');
+export const getAllTravels = () => axios.get('http://localhost:8000/api/travel');
 
-export const deleteTravel = (id) => axios.delete(`http://localhost:8080/api/travel/${id}`);
+export const deleteTravel = (id) => axios.delete(`http://localhost:8000/api/travel/${id}`);
 
-export const updateTravel = (id, travel) => axios.put(`http://localhost:8080/api/travel/${id}`, travel);
+export const updateTravel = (id, travel) => axios.put(`http://localhost:8000/api/travel/${id}`, travel);
 
-export const getTravel = (id) => axios.get(`http://localhost:8080/api/travel/${id}`);
+export const getTravel = (id) => axios.get(`http://localhost:8000/api/travel/${id}`);
 
-export const addCommentToTravel = (id, review) => axios.post(`http://localhost:8080/api/travel/review/${id}`, review);
+export const addCommentToTravel = (id, review) => axios.post(`http://localhost:8000/api/travel/review/${id}`, review);
 
-export const averageRatingFromService = () => axios.get(`http://localhost:8080/api/travel/rating/`);
+export const averageRatingFromService = () => axios.get(`http://localhost:8000/api/travel/rating/`);
